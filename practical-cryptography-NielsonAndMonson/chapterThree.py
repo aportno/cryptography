f3cfeac759846656513cdfbbbdfe8c6a8e745807
aportno <alexander.portno@gmail.com>
2021-06-14 12:57:16 -0400

    ch2

---
+++ practical-cryptography-NielsonAndMonson/chapterThree.py
@@ -0,0 +1,15 @@
+# NEVER USE: ECB IS NOT SECURE!
+
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+import os
+
+key = os.urandom(16) # random 128-bit key
+aesCipher = Cipher(algorithms.AES(key),
+                   modes.ECB(),
+                   backend=default_backend())
+aesEncryptor = aesCipher.encryptor()
+aesDecryptor = aesCipher.decryptor()
+
+
+print(key)
