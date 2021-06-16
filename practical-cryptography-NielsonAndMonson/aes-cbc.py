00aec93ae4974935344fd612ad45d05027f6cc9f
aportno <alexander.portno@gmail.com>
2021-06-16 11:49:33 -0400

    updates

--- practical-cryptography-NielsonAndMonson/aes-cbc.py
+++ practical-cryptography-NielsonAndMonson/aes-cbc.py
@@ -1,5 +1,6 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
 import os
 
 key = os.urandom(32)
@@ -11,4 +12,30 @@ aesCipher = Cipher(algorithms.AES(key),
 aesEncryptor = aesCipher.encryptor()
 aesDecryptor = aesCipher.decryptor()
 
+padder = padding.PKCS7(128).padder()
+unpadder = padding.PKCS7(128).unpadder()
 
+plaintexts = [
+    b"SHORT",
+    b"MEDIUM MEDIUM MEDIUM",
+    b"LONG LONG LONG LONG LONG LONG"
+]
+
+ciphertexts = []
+
+for message in plaintexts:
+    print(message)
+    padded_message = padder.update(message)
+    print(padded_message)
+
+    print(ciphertexts)
+    ciphertexts.append(aesEncryptor.update(padded_message))
+    print(ciphertexts)
+
+ciphertexts.append(aesEncryptor.update(padder.finalize()))
+
+for cipher in ciphertexts:
+    padded_message = aesDecryptor.update(cipher)
+    print("recovered", unpadder.update(padded_message))
+
+print("recovered", unpadder.finalize())
